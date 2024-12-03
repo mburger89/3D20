990023d6e270b56adf77367333f198f0b9582cf3
Max Burger <mburger89@me.com>
2024-12-03 09:17:08 -0800

    Initial Commit

---
+++ 3D20Tests/_D20Tests.swift
@@ -0,0 +1,17 @@
+//
+//  _D20Tests.swift
+//  3D20Tests
+//
+//  Created by Anson Burger on 12/3/24.
+//
+
+import Testing
+@testable import _D20
+
+struct _D20Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
