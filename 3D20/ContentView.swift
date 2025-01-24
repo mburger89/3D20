9699bfbad1dd7133a7e2406aae336e8e789e7fb3
Max Burger <mburger89@me.com>
2025-01-24 13:17:15 -0800

    adds bottom bar view and adds dice and dice_tray models

--- 3D20/ContentView.swift
+++ 3D20/ContentView.swift
@@ -7,34 +7,71 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
+
+@Observable
+class DiceData {
+    var dice_name: String = "D20_temp"
+    var dice_bool: Bool = false
+    var tray_name: String = "dice_tray"
+}
+
 
 struct ContentView : View {
-
+    @Bindable private var diceData: DiceData = DiceData()
     var body: some View {
-        RealityView { content in
+        ZStack {
+            RealityView { content in
+                async let tray = ModelEntity(named: diceData.tray_name)
+                if let tray = try? await tray {
+                    tray.setScale(SIMD3(0.0050, 0.0050, 0.0050), relativeTo: nil)
+                    tray.position = [0, 0, 0]
+                    tray.components.set(PhysicsBodyComponent())
+                    try? await tray.components.set(CollisionComponent(
+                        shapes: [ShapeResource.generateConvex(from: tray.model?.mesh ?? .generateBox(size: 1.0))],
+                        isStatic: true)
+                    )
+                    tray.collision?.mode = .colliding
+                    tray.collision?.filter = .default
+                    tray.physicsBody?.mode = .static
+                    tray.physicsBody?.isContinuousCollisionDetectionEnabled = true
+                }
+                async let dice = ModelEntity(named: diceData.dice_name)
+                if let dice = try? await dice {
+                    dice.setScale(SIMD3(0.010, 0.010, 0.010), relativeTo: nil)
+                    dice.position = [0, 0.2, 0]
+                    dice.components.set(PhysicsBodyComponent())
+                    try? await dice.components.set(CollisionComponent(shapes: [ShapeResource.generateConvex(from: dice.model?.mesh ?? .generateBox(size: 1.0))]))
+                    dice.collision?.mode = .colliding
+                    dice.collision?.filter = .default
+                    dice.physicsBody?.mode = .dynamic
+                    dice.physicsBody?.isContinuousCollisionDetectionEnabled = true
+                }
+                    // Create horizontal plane anchor for the content
+                let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.01, 0.01)))
+                try? await anchor.addChild(tray)
+                try? await anchor.addChild(dice)
+                    // Add the horizontal plane anchor to the scene
+                content.add(anchor)
 
-            // Create a cube model
-            let model = Entity()
-            let mesh = MeshResource.generateBox(size: 0.1, cornerRadius: 0.005)
-            let material = SimpleMaterial(color: .gray, roughness: 0.15, isMetallic: true)
-            model.components.set(ModelComponent(mesh: mesh, materials: [material]))
-            model.position = [0, 0.05, 0]
-
-            // Create horizontal plane anchor for the content
-            let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.2, 0.2)))
-            anchor.addChild(model)
-
-            // Add the horizontal plane anchor to the scene
-            content.add(anchor)
-
-            content.camera = .spatialTracking
-
-        }
-        .edgesIgnoringSafeArea(.all)
+                content.camera = .spatialTracking
+                
+            } update: { content in
+            }
+//            this didn't work will need to find another way to change skin.
+            VStack {
+                Spacer()
+                Button("Start") {
+                    diceData.dice_name = "normalMapTest_d20"
+                    diceData.dice_bool.toggle()
+                }.padding(30)
+            }
+            
+            }.edgesIgnoringSafeArea(.all)
     }
 
 }
 
-#Preview {
-    ContentView()
-}
+//#Preview {
+//    ContentView()
+//}
---
+++ 3D20/HUD.swift
@@ -0,0 +1,38 @@
+//
+//  HUD.swift
+//  3D20
+//
+//  Created by Anson Burger on 1/16/25.
+//
+
+import SwiftUI
+import SceneKit
+
+
+struct HUD: View {
+    
+
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                Button(action: {print("hello")}){
+                    Image(systemName: "gearshape.fill").font(.largeTitle)
+                }
+                Spacer()
+                Button(action:{print("hello")}) {
+                   
+                }
+                Spacer()
+                Button(action: {print("hello")}) {
+                   Image("skins")
+                }
+                Spacer()
+            }
+        }
+    }
+}
+#Preview {
+    HUD()
+}
