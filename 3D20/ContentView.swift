27e3206369ee5f2d95b51493b297d4aaaf15ff4a
Max Burger <mburger89@me.com>
2025-06-28 15:43:39 -0700

    Adds abilty to re-roll your dice. Also removes some unessary files

--- 3D20/ContentView.swift
+++ 3D20/ContentView.swift
@@ -14,17 +14,21 @@ class DiceData {
     var dice_name: String = "D20_temp"
     var dice_bool: Bool = false
     var tray_name: String = "dice_tray"
+    var dice_position: SIMD3<Float> = [0, 0.2, 0]
 }
 
 
 struct ContentView : View {
-    @Bindable private var diceData: DiceData = DiceData()
+    @Bindable var diceData: DiceData = DiceData()
+    @State private var isVisible: Bool = true
     var body: some View {
         ZStack {
             RealityView { content in
-                async let tray = ModelEntity(named: diceData.tray_name)
-                if let tray = try? await tray {
-                    tray.setScale(SIMD3(0.0050, 0.0050, 0.0050), relativeTo: nil)
+                // Create horizontal plane anchor for the content
+                let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.001, 0.001)))
+                anchor.name = "dice_anchor"
+                if let tray = try? await ModelEntity(named: diceData.tray_name) {
+                    tray.setScale(SIMD3(0.010, 0.010, 0.010), relativeTo: nil)
                     tray.position = [0, 0, 0]
                     tray.components.set(PhysicsBodyComponent())
                     try? await tray.components.set(CollisionComponent(
@@ -35,11 +39,12 @@ struct ContentView : View {
                     tray.collision?.filter = .default
                     tray.physicsBody?.mode = .static
                     tray.physicsBody?.isContinuousCollisionDetectionEnabled = true
+                    anchor.addChild(tray)
                 }
-                async let dice = ModelEntity(named: diceData.dice_name)
-                if let dice = try? await dice {
-                    dice.setScale(SIMD3(0.010, 0.010, 0.010), relativeTo: nil)
-                    dice.position = [0, 0.2, 0]
+               if let dice = try? await ModelEntity(named: diceData.dice_name) {
+                    dice.name = "die"
+                    dice.setScale(SIMD3(0.03, 0.03, 0.03), relativeTo: nil)
+                   dice.position = [0, 0.2, 0]
                     dice.components.set(PhysicsBodyComponent())
                     try? await dice.components.set(CollisionComponent(shapes: [ShapeResource.generateConvex(from: dice.model?.mesh ?? .generateBox(size: 1.0))]))
                     dice.collision?.mode = .colliding
@@ -47,27 +52,30 @@ struct ContentView : View {
                     dice.physicsBody?.mode = .dynamic
                     dice.physicsBody?.isContinuousCollisionDetectionEnabled = true
                     dice.transform.rotation = simd_quatf(angle: Float.random(in: 1.0 ..< 360.0), axis: SIMD3(1.0, 0.0, 0.0))
+                    anchor.addChild(dice)
                 }
-                    // Create horizontal plane anchor for the content
-                let anchor = AnchorEntity(.plane(.horizontal, classification: .any, minimumBounds: SIMD2<Float>(0.001, 0.001)))
-                try? await anchor.addChild(tray)
-                try? await anchor.addChild(dice)
-//                try? await content.add(dice)
-                    // Add the horizontal plane anchor to the scene
+                
                 content.add(anchor)
 
                 content.camera = .spatialTracking
                 
             } update: { content in
-                
+                if let DieEntity = content.entities.first(where: { $0.name == "dice_anchor" }) {
+                    if let die = DieEntity.children.first(where: { $0.name == "die" }) {
+                            die.position = diceData.dice_position
+                            die.transform.rotation = simd_quatf(angle: Float.random(in: 1.0 ..< 360.0), axis: SIMD3(1.0, 0.0, 0.0))
+                        }
+                    }
             }
 //            this didn't work will need to find another way to change skin.
             VStack {
                 Spacer()
-                Button("Start") {
-                    diceData.dice_name = "normalMapTest_d20"
-                    diceData.dice_bool.toggle()
-                }.padding(30)
+                Button("Roll") {
+//                    diceData.dice_name = "normalMapTest_d20"
+//                    diceData.dice_bool.toggle()
+//                    print(isVisible)
+                    diceData.dice_position = [0, 0.21, 0]
+                }.padding(30).buttonStyle(.borderedProminent)
             }
             
             }.edgesIgnoringSafeArea(.all)
